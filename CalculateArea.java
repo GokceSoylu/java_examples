087f53ed1f7c342c599bc148c230c0e0c04e20e6
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-09-02 23:31:03 +0300

    .

---
+++ CalculateArea.java
@@ -0,0 +1,10 @@
+public class CalculateArea 
+{
+    public static void main(String [] args)
+    {
+        int r=2;
+        int pi=3;
+        float area=(float)4/3*pi*r*r;
+        System.out.printf("the area of circle which has "+r+" radius is "+area);
+    }
+}
---
+++ calculate.java
@@ -0,0 +1,7 @@
+public class calculate
+{
+    public static void main(String[] args)
+    {
+        System.out.println(2*10/5+4-1);
+    }    
+}//" tırnak işareti" olmadan yazdığımız işlemi yapıp sonucunu yazdırıyor :)
