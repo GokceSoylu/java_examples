a23ce083a9aee8134102401e670e586fd3f9ee0e
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-09-02 21:59:33 +0300

    .

---
+++ first.java
@@ -0,0 +1,7 @@
+public class first
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome Necmiye :))");
+    }
+}
---
+++ printf_.java
@@ -0,0 +1,7 @@
+public class printf_
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("%s%n%s","Necmiye","SOYLU");
+    }   
+}//*  c=\n  %n
