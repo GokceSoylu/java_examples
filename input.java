7a35e4fba3ede543954c7a8967a5ca978383d7b6
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-09-03 15:09:59 +0300

    .

--- input.java
+++ input.java
@@ -1,14 +1,46 @@
 import java.util.Scanner;
 public class input 
 {
-    public static void main(String[] args)
+    public static void main(String args[])
     {
         int a,b;
+        float x,y;
+        double z,t;
+        char m;
+        String w; 
         Scanner ns=new Scanner(System.in);
-        System.out.printf("%na = ");
+        
+        //todo string ve cahr input'u bir tık farklı dikkat eddelim
+        //todo  Char
+        System.out.printf("%ncahr m = ");
+        m=ns.nextLine().charAt(0);
+        System.out.printf("m = %c%n++m = %c",m,++m);
+        
+        //todo  String
+        System.out.printf("%nstring w = ");
+        w=ns.nextLine();
+        System.out.printf("%nw = %s",w);
+
+        //todo  int
+        System.out.printf("%nint a = ");
         a=ns.nextInt();
-        System.out.printf("%nb = ");
+        System.out.printf("%nint b = ");
         b=ns.nextInt();
         System.out.printf("%na + b = %d",a+b);
+        
+        //todo  float
+        System.out.printf("%nflaot x = ");
+        x=ns.nextFloat();
+        System.out.printf("%nflaot y = ");
+        y=ns.nextFloat();
+        System.out.printf("%nx + y = %f",x+y);
+
+        //todo  double
+        System.out.printf("%ndouble z = ");
+        z=ns.nextDouble();
+        System.out.printf("%ndouble t = ");
+        t=ns.nextDouble();
+        System.out.printf("%nz + t = %f",z+t);
+
     }
 }
