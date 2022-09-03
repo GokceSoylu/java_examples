c106a1ce643d0a81c4134049becef3c255741f85
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-09-03 22:15:20 +0300

    .

---
+++ loops.java
@@ -0,0 +1,19 @@
+public class loops //döngü yapıları c ile aynı
+{
+    public static void main(String[] args)
+    {
+        int i, number, sum;
+        System.out.println(1%10);
+        for(i=10;i<20;i++)
+        {
+            sum=0;
+            number=i;
+            while(number>0)
+            {
+                sum+=number%10;
+                number=number/10;
+            }    
+            System.out.println(sum);  
+        }
+    }
+}
