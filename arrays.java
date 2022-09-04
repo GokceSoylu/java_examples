20212f8b2770d582628b16233cd2d3796ac26dfd
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-09-04 12:38:37 +0300

    .

--- arrays.java
+++ arrays.java
@@ -3,19 +3,19 @@ public class arrays
 {
     public static void main(String[] args)
     {
-        Scanner in=new Scanner(System.in);
+        Scanner input=new Scanner(System.in);
         int array[]=new int[10];//* C'den farkı bos dizinin içerisini 0 yapar :) 
         int array2[]={1,2,3,4,5,6,7,8,9,10}, i;
         for(i=0;i<10;i++)
         {
-            array[i]=in.nextInt();
+            array[i]=input.nextInt();
             array[i]+=array2[i];
         }  
         for(i=0;i<10;i++)
             System.out.printf("%d\t",array[i]);
-        
-            String s[]={"necmiye","sabriye","murat","behice"};
-            System.out.println(array2[0]+" "+s[0]+" "+array[0]); 
+        String s[]={"necmiye","sabriye","murat","behice"};
+        System.out.println("\n"+array2[0]+" "+s[0]+" "+array[0]);
+        input.close();
     }
 }
 //evet array'de c ile aynı :))
