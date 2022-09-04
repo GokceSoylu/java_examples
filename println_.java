01cbd6512a71b1b00787030cc55aacab9a704623
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-09-04 14:56:52 +0300

    println ve islemler

--- println_.java
+++ println_.java
@@ -8,6 +8,8 @@ public class println_
         System.out.println("the answer "+a+b);//string gibi degerleri yan yana yazdırıyor
         System.out.println("the answer "+(a+b));//parantez içinin işlemğini yapıp yazdırıyor
         System.out.println("the aswer "+a*b);// * + dan öncelikli --> ez cümle işlem önceliğine vardır
+        //! System.out.println("the answer "+a-b); hata verir çünkü işlem önceliği yok. Soldan yazdırmaya baslar aa yı string olarak ekler ama -  ifdesini tanımaz. 
+        System.out.println("the answer "+(a-b));
         System.out.println("the answer "+a/b);//tam bolme
         System.out.println("the answer "+(float)a/b);//ondaklıklı bolme
         System.out.println("the anwer "+(float)(a/b));//sonucu float yaptı, float bolmedi :))
@@ -16,7 +18,8 @@ public class println_
 /*  output
  *  the aswer 1020
  *  the aswer 30
- *  the aswer 200
+ *  the answer 200
+ *  the aswer -10
  *  the aswer 0
  *  the aswer 0.5
  *  the answer 0.0
