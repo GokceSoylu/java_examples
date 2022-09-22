027d4f288803bdb8c3b738d6ea8d67a8870d5e1b
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-09-22 14:43:24 +0300

    .

---
+++ method.java
@@ -0,0 +1,16 @@
+public class method 
+{
+    static int f(int a)
+    {
+        return 2*a;
+    }
+    public static void main(String[] Soylu)
+    {
+        System.out.println(f(g(10)));
+    }
+    static int g(int a)
+    {
+        return 2*a;
+    }
+}
+// methodlar hakkında: javada prototip olayı yok ister main den once yaz methodu iyster sonra hiç fark etmez prototipe ihtiyaç yokk:)
