76501e7a0ed477ee90624626a95eb52b8fd7e093
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-09-22 14:51:29 +0300

    .

---
+++ method2.java
@@ -0,0 +1,27 @@
+public class method2 
+{
+    public static void main(String[] Soylu)
+    {
+        int a=10;
+        double b=1.2;
+        char c='A';
+        f(a);
+        f(b);
+        f(c);
+    }     
+    static void f(int a)
+    {
+        System.out.println("int "+a);
+    } 
+    static void f(double b)
+    {
+        System.out.println("float " + b);
+    }
+    static void f(char c)
+    {
+        System.out.println("char "+ c);
+    }
+}
+// overloading 
+// √overriding 
+// tipsiz :))
